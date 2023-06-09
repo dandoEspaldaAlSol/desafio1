abdbcefd125473fe848ab6bcf2d6a990fb0955dd
Juan Manuel Godoy <103082706+dandoEspaldaAlSol@users.noreply.github.com>
2023-06-08 21:08:48 -0300

    Update productManager.js

--- productManager.js
+++ productManager.js
@@ -4,6 +4,9 @@ class ProductManager {
     #path
     constructor(path) {
         this.#path = path
+        if (!fs.existsSync(this.#path)) {
+            fs.promises.writeFile(this.#path, JSON.stringify([]));
+        }
     }
 
     read = () => {
@@ -11,9 +14,23 @@ class ProductManager {
             return fs.promises.readFile(this.#path, 'utf-8').then(r => JSON.parse(r))
         } else return []
     }
-
-
+    
+    updateProduct = async (productoUpdate) => {
+        let productos = await this.getProducts();
+        
+        let productoModificado = productos.find(prod => prod.id == productoUpdate.id);
+        productoModificado.title = (productoUpdate.title != null)? productoUpdate.title : productoModificado.title;
+        
+        await fs.promises.writeFile(this.#path, JSON.stringify(productos));
+    }
+    
+    // let producto = await this.getProductByIdIn(productoUpdate.id, productos);
     addProduct = async (title, description, price, thumbnail, code, stock) => {
+        if (await this.codeEstaRepetido(code)){
+            console.log("codigo repetido")
+            return
+        }
+
         const product = {
             title,
             description,
@@ -24,11 +41,22 @@ class ProductManager {
             id: await this.getNextID() // Asignar el ID obtenido mediante getNextID()
         };
     
+
         const list = await this.getProducts();
         list.push(product);
     
         // Escribir el archivo (o sobreescribirlo si ya existe)
         await fs.promises.writeFile(this.#path, JSON.stringify(list));
+    }
+
+    codeEstaRepetido = async (code) => {
+        let list = await this.getProducts();
+        let resultado = list.find(product => product.code == code)
+        if (resultado == undefined){
+            return false
+        }else{
+            return true
+        }
     }
 
     getNextID = async () => {
@@ -43,8 +71,8 @@ class ProductManager {
     }
 
     getProductById = async (id) => {
-        
         const list = await this.getProducts()
+        // return await this.getProductByIdIn(id, list);
         const resultado = list.find(product => product.id === id)
         if (resultado === undefined) {
             return "Producto no encontrado"
@@ -52,6 +80,8 @@ class ProductManager {
             return resultado
         }
     }
+
+
     
     deleteProduct = async (id) => {
         const list = await this.getProducts()
@@ -72,10 +102,10 @@ class ProductManager {
     getProducts = async () => {
         try {
             // Leer contenido del archivo
-            const data = await fs.promises.readFile(this.#path, 'utf-8')
-            const dataObj = JSON.parse(data) // Convertimos de string a objeto
+            const data = await this.read()
+            // const dataObj = JSON.parse(data) // Convertimos de string a objeto
 
-            return dataObj
+            return data;
         } catch (error) {
             // Si no existe el archivo, retornamos una lista vacia
             console.log('No se encontro el archivo, se devuelve vacio')
@@ -86,9 +116,24 @@ class ProductManager {
 }
 
 async function run() {
+
     const manager = new ProductManager('users.json')
     await manager.addProduct('zapatilla', 'calzado', 23, 'sin imagen', 01, 20)
     await manager.addProduct('mochila', 'indumentaria', 34, 'sin imagen',02, 40)
+
+    const productoModificado = {
+        title: "zapato",
+        // description,
+        // price,
+        // thumbnail,
+        // code,
+        // stock,
+        id: 2
+    };
+
+   await manager.updateProduct(productoModificado)
+    
+    
     console.log(await manager.getProducts())
     console.log(await manager.deleteProduct(1))
 
